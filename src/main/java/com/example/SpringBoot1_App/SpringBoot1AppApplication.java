5096ca162a287e52208a9e4e8bd5447b69ac3c0c
satyam146 <82771699+satyam146@users.noreply.github.com>
2023-01-27 01:47:06 +0530

    First Commit

---
+++ src/main/java/com/example/SpringBoot1_App/SpringBoot1AppApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot1_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot1AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot1AppApplication.class, args);
+	}
+
+}
