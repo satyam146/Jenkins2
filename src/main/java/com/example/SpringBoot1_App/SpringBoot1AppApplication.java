87178e876de10fe597bba8a8182a2f773e879eb4
satyam146 <82771699+satyam146@users.noreply.github.com>
2023-01-29 02:56:25 +0530

    Update SpringBoot1AppApplication.java

--- src/main/java/com/example/SpringBoot1_App/SpringBoot1AppApplication.java
+++ src/main/java/com/example/SpringBoot1_App/SpringBoot1AppApplication.java
@@ -4,15 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBoot1AppApplication implements commandLineRunner{
+public class SpringBoot1AppApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String args[]) throws Exception {
 		System.out.println("Hello World");
 	}
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootAppApplication.class, args);
-	}
+	
 	}
 
 
